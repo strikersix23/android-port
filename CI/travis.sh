2857b673e95f3bb736a359db8a3bce1c7747e00c
Ilya Zhuravlev <whatever@xyz.is>
2018-01-20 17:50:14 -0500

    travis: download latest cmake

--- CI/travis.sh
+++ CI/travis.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+wget https://cmake.org/files/v3.10/cmake-3.10.2-Linux-x86_64.tar.gz
+tar xvf cmake-*
+cd cmake-*
+export PATH=$(pwd)/bin:$PATH
+cd ..
+
 ./buildscripts/build.sh
 
 ./gradlew assembleDebug
